378c8f4bc60a2d580bf1a8b269f3fc5fb1369936
Robin Jacob Roy <robinjacobroy1@gmail.com>
2022-08-15 19:05:06 +0530

    remove csv creation
    As the images are moved from one folder to another no need to create csv file to save the sorted image names

--- sort_unsorted.py
+++ sort_unsorted.py
@@ -9,27 +9,12 @@ from pathlib import Path
 from os.path import exists 
 
 par_dir = '/home/robin/Work/fruitfly/to_predict/'
-csv_fln = '/home/robin/Work/fruitfly/done_images.csv'
-
-file_exists = exists(csv_fln) 
-if not file_exists:
-    print("new csv file created")
-    Path(csv_fln).touch()
-
-with open(csv_fln, 'r') as csvfile:
-    reader = csv.reader(csvfile)
-    temp_list = list(reader) 
-    read_list = temp_list if len(temp_list)==0 else temp_list[0]
 
 
 for fln in (glob("/home/robin/Work/fruitfly/unsorted/*", recursive=True)):
-    fname=fln.rpartition('/')[-1]
+    fname = fln.rpartition('/')[-1]
     name = fln.split('/')[-2]+' - '+fln.split('/')[-1]
-    
-    if fname in read_list:
-        continue
-    read_list.append(fname)
-        
+           
     img = cv2.imread(fln)
     cv2.namedWindow(name, cv2.WINDOW_NORMAL)
     cv2.imshow(name,img)
@@ -57,11 +42,6 @@ for fln in (glob("/home/robin/Work/fruitfly/unsorted/*", recursive=True)):
             shutil.move(fln, par_dir+'orientation/'+fname)
             break
         
-        if k==27:
-            # writing to csv file 
-            with open(csv_fln, 'w') as csvfile: 
-                csvwriter = csv.writer(csvfile) 
-                csvwriter.writerow(read_list)
-                
+        if k==27:             
             cv2.destroyAllWindows()
             exit(0)
